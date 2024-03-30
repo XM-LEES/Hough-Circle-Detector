c767994a6caec21895b23081d6aa45f728aacb90
unknown <limh2002@163.com>
2024-03-30 15:02:16 +0800

    Modify: Last version with class Hough_Detector

--- Launch.py
+++ Launch.py
@@ -1,16 +1,18 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QFileDialog, QHBoxLayout
-from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QFileDialog, QHBoxLayout, QSlider
+from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt
+# from hough_circle import detect_circles, display_circles
 from hough_circle import Hough_Detector
 
 class ImageViewer(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.Hough_Detector = None
-        
+        # self.Hough_Detector = None
+
+
     def initUI(self):
         # 设置窗口参数
         self.setWindowTitle('图像浏览器')
@@ -45,15 +47,37 @@ class ImageViewer(QWidget):
 
         # 布局和控件
         layout = QVBoxLayout()
-        # 添加一个 QLabel 控件用于显示图片
-        self.image_label = QLabel(self)
-        self.image_label.setAlignment(Qt.AlignCenter)
-        self.image_label.setFixedSize(800, 600)  # 设置固定大小
+
+        # # 滑块
+        # self.slider = QSlider(Qt.Horizontal)
+        # self.slider.setMinimum(0)
+        # self.slider.setMaximum(2)  # 三个固定位置
+        # self.slider.setTickInterval(1)
+        # self.slider.setSingleStep(1)  # 设置单步步长为1
+        # self.slider.setPageStep(1)    # 设置页步长为1，用于拖动时的增量
+        # self.slider.setTickPosition(QSlider.TicksBelow)
+        # self.slider.valueChanged.connect(self.on_slider_value_changed)
+        # self.slider.setFixedSize(200, 30)
+        # # self.slider.setTickLabels(["Position A", "Position B", "Position C"])  # 设置刻度的文本标签
+        # # self.slider.value()
         
-        # 添加一个 QLabel 控件用于显示提示信息
-        self.message_label = QLabel(self)
-        self.message_label.setAlignment(Qt.AlignCenter)
-        # self.message_label.setFixedSize(800, 100)  # 设置固定大小
+        # 添加 QLabel 控件用于显示图片
+        image_layout = QHBoxLayout()
+        self.original_image_label = QLabel(self)
+        self.original_image_label.setAlignment(Qt.AlignCenter)
+        self.original_image_label.setFixedSize(600, 450)  # 设置固定大小
+        self.detected_image_label = QLabel(self)
+        self.detected_image_label.setAlignment(Qt.AlignCenter)
+        self.detected_image_label.setFixedSize(600, 450)  # 设置固定大小
+        
+        # 添加 QLabel 控件用于显示提示信息
+        message_layout = QHBoxLayout()
+        self.message_label1 = QLabel(self)
+        self.message_label1.setAlignment(Qt.AlignCenter)
+        self.message_label2 = QLabel(self)
+        self.message_label2.setAlignment(Qt.AlignCenter)
+        self.message_label3 = QLabel(self)
+        self.message_label3.setAlignment(Qt.AlignCenter)
 
         # 添加按钮
         button_layout = QHBoxLayout()
@@ -62,45 +86,69 @@ class ImageViewer(QWidget):
         self.btn_process = QPushButton('处理图像', self)
         self.btn_auto_process = QPushButton('自动批量处理', self)
         self.btn_open_folder = QPushButton('选择文件夹', self)
-
         # 事件绑定
         self.btn_previous.clicked.connect(self.show_previous_image)
         self.btn_next.clicked.connect(self.show_next_image)
         self.btn_process.clicked.connect(self.process_image)
         self.btn_auto_process.clicked.connect(self.auto_process_image)
         self.btn_open_folder.clicked.connect(self.open_folder)
-
+        
         # 将控件添加到布局
+        image_layout.addWidget(self.original_image_label)
+        image_layout.addWidget(self.detected_image_label)
+        message_layout.addWidget(self.message_label1)
+        message_layout.addWidget(self.message_label2)
         button_layout.addWidget(self.btn_previous)
         button_layout.addWidget(self.btn_next)
         button_layout.addWidget(self.btn_process)
         button_layout.addWidget(self.btn_auto_process)
 
-        # layout.addWidget(self.message_label)
-        layout.addWidget(self.image_label)
-        layout.addWidget(self.message_label)
+        layout.addLayout(image_layout)
+        layout.addLayout(message_layout)
+        layout.addWidget(self.message_label3)
         layout.addWidget(self.btn_open_folder)
         layout.addLayout(button_layout)
+        # layout.addWidget(self.slider)
         layout.setAlignment(Qt.AlignCenter)  # 居中布局
 
         self.setLayout(layout)
 
-    def show_message(self, message):
-        self.message_label.setText(message)
+
+    # def on_slider_value_changed(self, value):
+    #     if value == 0:
+    #         self.show_message1("Slider value changed to A")
+    #     elif value == 1:
+    #         self.show_message1("Slider value changed to B")
+    #     elif value == 2:
+    #         self.show_message1("Slider value changed to C")
+
+
+    def show_message1(self, message):
+        self.message_label1.setText(message)
+
+    def show_message2(self, message):
+        self.message_label2.setText(message)
+
+    def show_message3(self, message):
+        self.message_label3.setText(message)
 
     def open_folder(self):
         folder_path = QFileDialog.getExistingDirectory(self, "选择文件夹")
         if folder_path:
             self.images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
-            self.modified = [False for _ in range(len(self.images))]
+            self.modified = [None for _ in range(len(self.images))]
             self.current_image_index = 0
+            self.show_message3(f"当前文件夹：{folder_path}")
             self.show_image()
-            self.show_message("打开文件夹：" + folder_path)
 
     def show_image(self):
         if self.images:
             pixmap = QPixmap(self.images[self.current_image_index])
-            self.image_label.setPixmap(pixmap.scaled(self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
+            self.original_image_label.setPixmap(pixmap.scaled(self.original_image_label.width(), self.original_image_label.height(), Qt.KeepAspectRatio))
+            self.show_message1("当前图片：" + self.images[self.current_image_index])
+            self.show_message3(f"{self.current_image_index + 1} / {len(self.images)}")
+        else:
+            self.show_message1("文件夹下无图片")
 
     def show_previous_image(self):
         if self.images:
@@ -114,23 +162,35 @@ class ImageViewer(QWidget):
 
     def process_image(self):
         if self.images:
-            # 在此添加图像处理逻辑
-            if self.modified[self.current_image_index]:
+
+            if self.modified[self.current_image_index] != None:
                 return
 
-            self.modified[self.current_image_index] = True
+            # self.modified[self.current_image_index] = True
 
             image_path = self.images[self.current_image_index]
+
             self.Hough_Detector = Hough_Detector(image_path)
-            
-            circles = self.Hough_Detector.detect_circles()
-            self.Hough_Detector.display_circles(circles)
+            circles, processed_image = self.Hough_Detector.detect_circles()
+
+            # circles = detect_circles(image_path=image_path)
+            # processed_image = display_circles(image_path, circles)
+
+
+            # 将 OpenCV 图像转换为 QImage
+            height, width, _ = processed_image.shape
+            bytes_per_line = 3 * width
+            q_image = QImage(processed_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            # 将 QImage 转换为 QPixmap
+            pixmap = QPixmap.fromImage(q_image)
+            # 显示处理后的图像
+            self.detected_image_label.setPixmap(pixmap.scaled(self.detected_image_label.width(), self.detected_image_label.height(), Qt.KeepAspectRatio))
 
             print("处理图像: " + image_path)
 
+
     def auto_process_image(self):
         if self.images:
-            # 在此添加图像处理逻辑
             for i in range(len(self.images)):
                 self.show_image()
                 self.process_image()
--- hough_circle.py
+++ hough_circle.py
@@ -1,6 +1,43 @@
 import cv2
 
 
+
+# def detect_circles(image_path, bilateral_diameter=15, sigma_color=15, sigma_space=50,
+#                     dp=1, minDist=10, param1=120, param2=60, minRadius=40, maxRadius=60):
+
+#     image_path = image_path
+#     img = cv2.imread(image_path)
+#     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     # 进行双边滤波
+#     bilateral = cv2.bilateralFilter(img_gray, bilateral_diameter, sigma_color, sigma_space)
+#     # hough圆变换
+#     cimg = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+#     circles = cv2.HoughCircles(image=bilateral, method=cv2.HOUGH_GRADIENT, dp=dp, minDist=minDist, 
+#                                 param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+#     return circles
+
+# def display_circles(image_path, circles):
+#     image_path = image_path
+#     img = cv2.imread(image_path)
+#     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#     if circles is not None:
+#         cimg = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+#         circles = circles[0, :].astype(int)
+#         for i in circles:
+#             cv2.circle(img=cimg, center=(i[0], i[1]), radius=i[2], color=(0, 255, 0), thickness=2)
+#             cv2.circle(img=cimg, center=(i[0], i[1]), radius=2, color=(0, 0, 255), thickness=3)
+        
+#         return cimg
+#         # cv2.imshow('Circles Detected', cimg)
+#         # cv2.waitKey(0)
+#         # cv2.destroyAllWindows()
+#     else:
+#         print("No circles detected.")
+
+
+
+
 class Hough_Detector(object):
     def __init__(self, image_path):
         self.image_path = image_path
@@ -12,20 +49,17 @@ class Hough_Detector(object):
         # 进行双边滤波
         bilateral = cv2.bilateralFilter(self.img_gray, bilateral_diameter, sigma_color, sigma_space)
         # hough圆变换
-        cimg = cv2.cvtColor(self.img_gray, cv2.COLOR_GRAY2BGR)
         circles = cv2.HoughCircles(image=bilateral, method=cv2.HOUGH_GRADIENT, dp=dp, minDist=minDist, 
                                     param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
-        return circles
     
-    def display_circles(self, circles):
         if circles is not None:
             cimg = cv2.cvtColor(self.img_gray, cv2.COLOR_GRAY2BGR)
             circles = circles[0, :].astype(int)
             for i in circles:
                 cv2.circle(img=cimg, center=(i[0], i[1]), radius=i[2], color=(0, 255, 0), thickness=2)
                 cv2.circle(img=cimg, center=(i[0], i[1]), radius=2, color=(0, 0, 255), thickness=3)
-            cv2.imshow('Circles Detected', cimg)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        else:
-            print("No circles detected.")
+            return circles, cimg
+
+        # else:
+        #     print("No circles detected.")
+        #     return None
